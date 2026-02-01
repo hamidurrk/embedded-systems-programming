ca358bd70b776db08e8a22d9d7a182d89056b45c
hamidurrk <hamidurrk@gmail.com>
2026-02-01 16:35:36 +0200

    Added initial implementation of elevator control system with GPIO configuration
    - Created avr_gpio.h and bit_ops.h for GPIO handling and bit manipulation.
    - Implemented board_config_2560.h for pin mappings.
    - Developed main.c for elevator logic and state machine.
    - Added pin_config.c and pin_config.h for button and LED configurations.
    - Introduced uart.c and uart.h for UART communication setup.

---
+++ Week4Ex1/Week4Ex1/avr_gpio.h
@@ -0,0 +1,52 @@
+/*
+ * avr_gpio.h
+ *
+ * Created: 1/26/2026 11:04:28 AM
+ *  Author: Hamidur
+ */ 
+
+
+#ifndef AVR_GPIO_H_
+#define AVR_GPIO_H_
+#include <stdio.h>
+#include <avr/io.h>
+#include "bit_ops.h"
+typedef struct avr_gpio_tag {
+	__typeof__(PORTA) *port;
+	__typeof__(DDRA) *direction;
+	__typeof__(PA0) pin_offset;
+	__typeof__(PINA) *input;
+} avr_gpio_t;
+
+static void set_as_output(avr_gpio_t * gpio_pin) {
+	// Output
+	SET_BIT(*(gpio_pin->direction), gpio_pin->pin_offset);
+}
+static void set_as_input_floating(avr_gpio_t * gpio_pin) {
+	// Input
+	CLEAR_BIT(*(gpio_pin->direction), gpio_pin->pin_offset);
+	// No pull-up
+	CLEAR_BIT(*(gpio_pin->port), gpio_pin->pin_offset);
+}
+static void set_as_input_pull_up(avr_gpio_t * gpio_pin) {
+	// Input
+	CLEAR_BIT(*(gpio_pin->direction), gpio_pin->pin_offset);
+	// With pull-up
+	SET_BIT(*(gpio_pin->port), gpio_pin->pin_offset);
+}
+
+
+static void set_gpio(avr_gpio_t * gpio_pin) {
+	SET_BIT(*(gpio_pin->port), gpio_pin->pin_offset);
+}
+static void clear_gpio(avr_gpio_t * gpio_pin) {
+	CLEAR_BIT(*(gpio_pin->port), gpio_pin->pin_offset);
+}
+
+static uint8_t read_gpio(avr_gpio_t * gpio_pin) {
+	uint8_t result = READ_BIT(*(gpio_pin->input), gpio_pin->pin_offset);
+	printf("reading gpio pin input %d result %d \r\n", gpio_pin->pin_offset, result);
+	return result;
+}
+
+#endif /* AVR_GPIO_H_ */
